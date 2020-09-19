71b859d792e432da674366a9d4e1571e2259cc66
GokulGamer <61051206+GokulGamer@users.noreply.github.com>
2020-09-19 14:52:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands 
 
-client = discord.Client()
+client = commands.Bot(command_prefix="g!")
 
 @client.event
 async def on_message(message):
@@ -19,5 +19,18 @@ async def on_message(message):
         
         await message.channel.purge(limit=1)
 
+@client.event
+async def on_message_join(member):
+    print(f'{member} has joined NGL we are very happy to have you!')
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left the server, and its sed!@')
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+
 client.run('NzQ3Njk3ODU1MTk5OTY5Mjgx.X0Sp5A.Q--ejtIyfw579qI6IJ4NDUsJX70')
+
 
