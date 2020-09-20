fc22705de4d86f08715d338b4ebb1c7bc1774cd9
GokulGamer <61051206+GokulGamer@users.noreply.github.com>
2020-09-20 08:44:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,21 +3,28 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix="g!")
 
+import discord
+from discord.ext import commands 
+
+client = commands.Bot(command_prefix="g!")
+
 @client.event
 async def on_message(message):
     message.content.lower()
     if message.author == client.user:
         return
-    if message.content.startswith("hi"):
+    elif message.content.startswith("hi"):
 
         if str(message.author) == "CodePoltergeist#8379": 
             await message.channel.send("Hello " + str(message.author) + "!")
         else:
             await message.channel.send("Heya dude, Wassup?")
-    
-    if str(message.channel) == "pictures" and message.content != "":
-        
+
+    elif str(message.channel) == "pictures" and message.content != "":
         await message.channel.purge(limit=1)
+
+    else:
+        await client.process_commands(message)
 
 @client.event
 async def on_message_join(member):
@@ -25,12 +32,14 @@ async def on_message_join(member):
 
 @client.event
 async def on_member_remove(member):
-    print(f'{member} has left the server, and its sed!@')
+    print(f'{member} has left the server, and its sed!')
 
 @client.command()
 async def ping(ctx):
     await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
 
 client.run('NzQ3Njk3ODU1MTk5OTY5Mjgx.X0Sp5A.Q--ejtIyfw579qI6IJ4NDUsJX70')
+
+
 
 
